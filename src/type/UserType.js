d8e80cd99160daee48255a2669efd0ad3188fac6
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2018-05-12 16:13:55 -0300

    adds role to user type

--- src/type/UserType.js
+++ src/type/UserType.js
@@ -1,16 +1,16 @@
 // @flow
 
-import {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLBoolean,
-} from 'graphql';
-import {
-  globalIdField,
-} from 'graphql-relay';
-import {
-  NodeInterface,
-} from '../interface/NodeInterface';
+import { GraphQLObjectType, GraphQLString, GraphQLBoolean } from 'graphql';
+import { globalIdField } from 'graphql-relay';
+import { NodeInterface } from '../interface/NodeInterface';
+
+type UserType = {
+  _id: string,
+  name: string,
+  email: string,
+  role: string,
+  active: string,
+};
 
 export default new GraphQLObjectType({
   name: 'User',
@@ -19,19 +19,23 @@ export default new GraphQLObjectType({
     id: globalIdField('User'),
     _id: {
       type: GraphQLString,
-      resolve: user => user._id,
+      resolve: (user: UserType) => user._id,
     },
     name: {
       type: GraphQLString,
-      resolve: user => user.name,
+      resolve: (user: UserType) => user.name,
     },
     email: {
       type: GraphQLString,
-      resolve: user => user.email,
+      resolve: (user: UserType) => user.email,
+    },
+    role: {
+      type: GraphQLString,
+      resolve: (user: UserType) => user.role,
     },
     active: {
       type: GraphQLBoolean,
-      resolve: user => user.active,
+      resolve: (user: UserType) => user.active,
     },
   }),
   interfaces: () => [NodeInterface],
