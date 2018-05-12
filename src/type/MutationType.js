344aad4efec72d166a1a6e6025aa92d2c17774ee
Luiz Fernando Sousa Camargo <luizepauloxd@gmail.com>
2018-05-12 20:35:52 -0300

    exposing mutations to the mutation type

--- src/type/MutationType.js
+++ src/type/MutationType.js
@@ -5,13 +5,20 @@ import { GraphQLObjectType } from 'graphql';
 import LoginEmail from '../mutation/LoginEmailMutation';
 import RegisterEmail from '../mutation/RegisterEmailMutation';
 import ChangePassword from '../mutation/ChangePasswordMutation';
+import AddEvent from '../mutation/AddEventMutation';
+import AttendToEvent from '../mutation/AttendToEventMutation';
+import CantGoToEvent from '../mutation/CantGoToEventMutation';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
-		// auth
+    // auth
     LoginEmail,
     RegisterEmail,
     ChangePassword,
+    // events
+    AddEvent,
+    AttendToEvent,
+    CantGoToEvent,
   }),
 });
