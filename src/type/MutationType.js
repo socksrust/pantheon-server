6561c11cd1de5d92e746421701059c4acce38ddf
jaburcodes <guilhermejabur@outlook.com>
2018-05-13 14:16:51 -0300

    add AddEvent Mutation to Mutation type

--- src/type/MutationType.js
+++ src/type/MutationType.js
@@ -5,13 +5,17 @@ import { GraphQLObjectType } from 'graphql';
 import LoginEmail from '../mutation/LoginEmailMutation';
 import RegisterEmail from '../mutation/RegisterEmailMutation';
 import ChangePassword from '../mutation/ChangePasswordMutation';
+import AddEvent from '../mutation/AddEventMutation';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: () => ({
-		// auth
+    // auth
     LoginEmail,
     RegisterEmail,
     ChangePassword,
+
+    // event
+    AddEvent,
   }),
 });
